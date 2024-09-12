3d879dea655f002fb5d2f4d9497363b2fc8ca269
Daniel Elias Fonseca Rumin <43758059+MrBElga@users.noreply.github.com>
2024-09-12 08:23:31 -0300

    Update Filtros.cs

--- ProcessamentoImagens/ProcessamentoImagens/Filtros.cs
+++ ProcessamentoImagens/ProcessamentoImagens/Filtros.cs
@@ -7,6 +7,229 @@ namespace ProcessamentoImagens
 {
     class Filtros
     {
+               //sem acesso direto a memoria
+        public static void convert_to_gray(Bitmap imageBitmapSrc, Bitmap imageBitmapDest)
+        {
+            int width = imageBitmapSrc.Width;
+            int height = imageBitmapSrc.Height;
+            int r, g, b;
+            Int32 gs;
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    //obtendo a cor do pixel
+                    Color cor = imageBitmapSrc.GetPixel(x, y);
+
+                    r = cor.R;
+                    g = cor.G;
+                    b = cor.B;
+                    gs = (Int32)(r * 0.2990 + g * 0.5870 + b * 0.1140);
+
+                    //nova cor
+                    Color newcolor = Color.FromArgb(gs, gs, gs);
+
+                    imageBitmapDest.SetPixel(x, y, newcolor);
+                }
+            }
+        }
+
+        //sem acesso direito a memoria
+        public static void negativo(Bitmap imageBitmapSrc, Bitmap imageBitmapDest)
+        {
+            int width = imageBitmapSrc.Width;
+            int height = imageBitmapSrc.Height;
+            int r, g, b;
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    //obtendo a cor do pixel
+                    Color cor = imageBitmapSrc.GetPixel(x, y);
+
+                    r = cor.R;
+                    g = cor.G;
+                    b = cor.B;
+
+                    //nova cor
+                    Color newcolor = Color.FromArgb(255 - r, 255 - g, 255 - b);
+
+                    imageBitmapDest.SetPixel(x, y, newcolor);
+                }
+            }
+        }
+
+        //com acesso direto a memória
+        public static void convert_to_grayDMA(Bitmap imageBitmapSrc, Bitmap imageBitmapDest)
+        {
+            int width = imageBitmapSrc.Width;
+            int height = imageBitmapSrc.Height;
+            int pixelSize = 3;
+            Int32 gs;
+
+            //lock dados bitmap origem
+            BitmapData bitmapDataSrc = imageBitmapSrc.LockBits(new Rectangle(0, 0, width, height),
+                ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            //lock dados bitmap destino
+            BitmapData bitmapDataDst = imageBitmapDest.LockBits(new Rectangle(0, 0, width, height),
+                ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+
+            int padding = bitmapDataSrc.Stride - (width * pixelSize);
+
+            unsafe
+            {
+                byte* src = (byte*)bitmapDataSrc.Scan0.ToPointer();
+                byte* dst = (byte*)bitmapDataDst.Scan0.ToPointer();
+
+                int r, g, b;
+                for (int y = 0; y < height; y++)
+                {
+                    for (int x = 0; x < width; x++)
+                    {
+                        b = *(src++); //está armazenado dessa forma: b g r 
+                        g = *(src++);
+                        r = *(src++);
+                        gs = (Int32)(r * 0.2990 + g * 0.5870 + b * 0.1140);
+                        *(dst++) = (byte)gs;
+                        *(dst++) = (byte)gs;
+                        *(dst++) = (byte)gs;
+                    }
+                    src += padding;
+                    dst += padding;
+                }
+            }
+            //unlock imagem origem
+            imageBitmapSrc.UnlockBits(bitmapDataSrc);
+            //unlock imagem destino
+            imageBitmapDest.UnlockBits(bitmapDataDst);
+        }
+
+        //com acesso direito a memoria
+        public static void negativoDMA(Bitmap imageBitmapSrc, Bitmap imageBitmapDest)
+        {
+            int width = imageBitmapSrc.Width;
+            int height = imageBitmapSrc.Height;
+            int pixelSize = 3;
+
+            //lock dados bitmap origem 
+            BitmapData bitmapDataSrc = imageBitmapSrc.LockBits(new Rectangle(0, 0, width, height),
+                ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            //lock dados bitmap destino
+            BitmapData bitmapDataDst = imageBitmapDest.LockBits(new Rectangle(0, 0, width, height),
+                ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+
+            int padding = bitmapDataSrc.Stride - (width * pixelSize);
+
+            unsafe
+            {
+                byte* src1 = (byte*)bitmapDataSrc.Scan0.ToPointer();
+                byte* dst = (byte*)bitmapDataDst.Scan0.ToPointer();
+
+                int r, g, b;
+                for (int y = 0; y < height; y++)
+                {
+                    for (int x = 0; x < width; x++)
+                    {
+                        b = *(src1++); //está armazenado dessa forma: b g r 
+                        g = *(src1++);
+                        r = *(src1++);
+
+                        *(dst++) = (byte)(255 - b);
+                        *(dst++) = (byte)(255 - g);
+                        *(dst++) = (byte)(255 - r);
+                    }
+                    src1 += padding;
+                    dst += padding;
+                }
+            }
+            //unlock imagem origem 
+            imageBitmapSrc.UnlockBits(bitmapDataSrc);
+            //unlock imagem destino
+            imageBitmapDest.UnlockBits(bitmapDataDst);
+        }
+
+
+       
+
+        //espelho vertical
+        public static void verticalSemDMA(Bitmap imageBitmapSrc, Bitmap imageBitmapDest)
+        {
+            int width = imageBitmapSrc.Width;
+            int height = imageBitmapSrc.Height;
+            int r, g, b, Raux, Baux, Gaux;
+            for (int y = 0; y < height; y++)
+            {
+                int widthaux = width - 1;
+                for (int x = 0; x < width; x++)
+                {
+
+                    //obtendo a cor do pixel
+
+                    Color cor = imageBitmapSrc.GetPixel(x, y);
+                    Color coraux = imageBitmapSrc.GetPixel(widthaux, y);
+
+                    r = cor.R;
+                    g = cor.G;
+                    b = cor.B;
+
+                    Raux = coraux.R;
+                    Gaux = coraux.G;
+                    Baux = coraux.B;
+
+                    //nova cor
+                    Color newcolor = Color.FromArgb(r, g, b);
+
+                    imageBitmapDest.SetPixel(widthaux, y, newcolor);
+                    newcolor = Color.FromArgb(Raux, Gaux, Baux);
+                    imageBitmapDest.SetPixel(x, y, newcolor);
+                    widthaux--;
+
+                }
+
+            }
+        }
+
+        //espelho horizontal
+        public static void horizontalSemDMA(Bitmap imageBitmapSrc, Bitmap imageBitmapDest)
+        {
+            int width = imageBitmapSrc.Width;
+            int height = imageBitmapSrc.Height;
+            int r, g, b, Raux, Baux, Gaux;
+            int heightaux = height - 1;
+            for (int y = 0; y < height; y++)
+            {
+
+                for (int x = 0; x < width; x++)
+                {
+
+                    //obtendo a cor do pixel
+
+                    Color cor = imageBitmapSrc.GetPixel(x, y);
+                    Color coraux = imageBitmapSrc.GetPixel(x, heightaux);
+
+                    r = cor.R;
+                    g = cor.G;
+                    b = cor.B;
+
+                    Raux = coraux.R;
+                    Gaux = coraux.G;
+                    Baux = coraux.B;
+
+                    //nova cor
+                    Color newcolor = Color.FromArgb(r, g, b);
+
+                    imageBitmapDest.SetPixel(x, heightaux, newcolor);
+                    newcolor = Color.FromArgb(Raux, Gaux, Baux);
+                    imageBitmapDest.SetPixel(x, y, newcolor);
+
+
+                }
+                heightaux--;
+            }
+        }
+
         public static void ZhangSuen(Bitmap imageBitmapSrc, Bitmap imageBitmapDest)
         {
             int width = imageBitmapSrc.Width;
