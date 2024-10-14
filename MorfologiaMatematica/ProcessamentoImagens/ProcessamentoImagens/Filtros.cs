28e9b241836fbcd7df498fa089bfa18706efcb9e
Daniel Elias Fonseca Rumin <43758059+MrBElga@users.noreply.github.com>
2024-10-13 21:37:27 -0300

    feat: dilatação & erosão

---
+++ MorfologiaMatematica/ProcessamentoImagens/ProcessamentoImagens/Filtros.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using static System.Net.Mime.MediaTypeNames;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace ProcessamentoImagens
+{
+    class Filtros
+    {
+
+        public static Bitmap Dilatacao(Bitmap imageSrc)
+        {
+            Bitmap imageDest = (Bitmap)imageSrc.Clone();
+         
+            int width = imageSrc.Width;
+            int height = imageSrc.Height;
+
+            for (int x = 1; x < width - 1; x++)
+            {
+                for (int y = 1; y < height - 1; y++)
+                {
+                    if (imageSrc.GetPixel(x, y).R == 0)
+                    {
+                        //mascara
+                        //0 eh o preto
+                        //{1,1,1}
+                        //{1,0,1}
+                        //{1,1,1}
+                        if(imageSrc.GetPixel(x , y).R == 0)
+                        {
+                            imageDest.SetPixel(x - 1, y, Color.Black);
+                            imageDest.SetPixel(x + 1, y, Color.Black);
+                            imageDest.SetPixel(x, y - 1, Color.Black);
+                            imageDest.SetPixel(x, y + 1, Color.Black);
+                            imageDest.SetPixel(x+1, y - 1, Color.Black);
+                            imageDest.SetPixel(x + 1, y + 1, Color.Black); 
+                            imageDest.SetPixel(x - 1, y - 1, Color.Black);
+                            imageDest.SetPixel(x - 1, y + 1, Color.Black);
+                        }
+
+                    }
+                }
+            }
+
+            return imageDest;
+        }
+
+        public static Bitmap Erosao(Bitmap imageSrc)
+        {
+            Bitmap imageDest = new Bitmap(imageSrc.Width, imageSrc.Height);
+            int width = imageSrc.Width;
+            int height = imageSrc.Height;
+
+            for (int x = 1; x < width - 1; x++)
+            {
+                for (int y = 1; y < height - 1; y++)
+                {
+                    //mascara
+                    //0 eh o preto
+                    //{1,1,1}
+                    //{1,0,1}
+                    //{1,1,1}
+                    if(imageSrc.GetPixel(x, y).R == 0)
+                    {
+                        if (imageSrc.GetPixel(x - 1, y).R == 255 || imageSrc.GetPixel(x + 1, y).R == 255 || imageSrc.GetPixel(x, y - 1).R == 255 || imageSrc.GetPixel(x, y + 1).R == 255)
+                        {
+                            imageDest.SetPixel(x, y, Color.White);
+                        }
+                        else
+                        {
+                            imageDest.SetPixel(x, y, Color.Black);
+                        }
+                    }
+                    else
+                    {
+                        imageDest.SetPixel(x, y, Color.White);
+                    }
+
+
+                }
+            }
+
+            return imageDest;
+        }
+
+
+
+
+    }
+}
---
+++ MorfologiaMatematica/ProcessamentoImagens/ProcessamentoImagens/frmPrincipal.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace ProcessamentoImagens
+{
+    public partial class frmPrincipal : Form
+    {
+        private Image image;
+        private Bitmap imageBitmap;
+
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAbrirImagem_Click(object sender, EventArgs e)
+        {
+            openFileDialog.FileName = "";
+            openFileDialog.Filter = "Arquivos de Imagem (*.jpg;*.gif;*.bmp;*.png)|*.jpg;*.gif;*.bmp;*.png";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                image = Image.FromFile(openFileDialog.FileName);
+                pictBoxImg1.Image = image;
+                pictBoxImg1.SizeMode = PictureBoxSizeMode.Normal;
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            pictBoxImg1.Image = null;
+            pictBoxImg2.Image = null;
+        }
+        /*
+        private void btnLuminanciaSemDMA_Click(object sender, EventArgs e)
+        {
+            Bitmap imgDest = new Bitmap(image);
+            imageBitmap = (Bitmap)image;
+            Filtros.convert_to_gray(imageBitmap, imgDest);
+            pictBoxImg2.Image = imgDest;
+        }
+        */
+        private void dilatacao_Click(object sender, EventArgs e)
+        {
+            // Carregar a imagem original de algum caminho (ou pode ser de um PictureBox)
+            Bitmap imgDest = new Bitmap(image);
+            imageBitmap = (Bitmap)image;
+            // Chamar o método de dilatação da classe Filtros
+            Bitmap imageDest = Filtros.Dilatacao(imageBitmap);
+
+            // Exibir a imagem dilatada em um PictureBox (substitua pictureBoxResultado pelo seu PictureBox)
+            pictBoxImg2.Image = imageDest;
+            try
+            {
+                imageDest.Save("caminho_para_imagem_resultado1.png", ImageFormat.Png);
+            }
+            catch (ExternalException ex)
+            {
+                MessageBox.Show($"Failed to save image: {ex.Message}");
+            }
+            // Ou salvar a imagem processada
+         
+        }
+
+        private void erosao_Click(object sender, EventArgs e)
+        {
+           
+            Bitmap imgDest = new Bitmap(image);
+            imageBitmap = (Bitmap)image;
+            Bitmap imageDest = Filtros.Erosao(imageBitmap);
+            pictBoxImg2.Image = imageDest;
+            try
+            {
+                imageDest.Save("caminho_para_imagem_resultado2.Png", ImageFormat.Png);
+            }
+            catch (ExternalException ex)
+            {
+                MessageBox.Show($"Failed to save image: {ex.Message}");
+            }
+         
+        }
+    }
+}
